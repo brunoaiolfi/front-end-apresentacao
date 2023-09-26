4d5da470494256ec0e7ae7d30b81bc69717c8b5a
Bruno Aiolfi <bruno.200722@alunosatc.edu.br>
2023-09-26 08:42:41 -0300

    Add files via upload

--- src/components/forms/ModalLogon/index.tsx
+++ src/components/forms/ModalLogon/index.tsx
@@ -1,7 +1,11 @@
-import { Box, Button, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
 import { Controller, useForm } from "react-hook-form";
 import * as Yup from 'yup';
 import { yupResolver } from "@hookform/resolvers/yup";
+import { api } from "../../../infra/api";
+import { User } from "../../../@types/user";
+import { toast } from 'react-toastify';
+import { useState } from "react";
 
 interface ModalLogonProps {
     visible: boolean;
@@ -15,19 +19,33 @@ interface ILogon {
 }
 
 const schema = Yup.object().shape({
-    name: Yup.string().required('Nome obrigatório'),
-    username: Yup.string().required('Nome de usuário obrigatório'),
-    password: Yup.string().required('Senha obrigatória'),
+    name: Yup.string().min(5, 'Nome precisa ter no mínimo 5 caracteres!').required('Nome obrigatório'),
+    username: Yup.string().min(5, 'Nome de usuário precisa ter no mínimo 5 caracteres!').required('Nome de usuário obrigatório'),
+    password: Yup.string().min(5, 'Senha precisa ter no mínimo 5 caracteres!').required('Senha obrigatória'),
 });
 
 export function ModalLogon({ visible = false, onChangeVisibility }: ModalLogonProps) {
+
+    const [isLoading, setIsLoading] = useState(false);
 
     const { control, handleSubmit, formState: { errors } } = useForm<ILogon>({
         resolver: yupResolver(schema)
     })
 
     async function handleLogon(dto: ILogon) {
-        console.log(dto);
+        try {
+            setIsLoading(true);
+            const { data } = await api.post<User>('/users/create', dto);
+
+            if (data.id) {
+                toast.success('Usuário cadastrado com sucesso!')
+                onChangeVisibility();
+            }
+        } catch (error: any) {
+            toast.error(error.response.data)
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -52,6 +70,13 @@ export function ModalLogon({ visible = false, onChangeVisibility }: ModalLogonPr
                         <Box
                             width={'100%'}
                         >
+                            <Text
+                                fontSize={'sm'}
+                                color={'red.400'}
+                                marginBottom={2}
+                            >
+                                {errors.name?.message}
+                            </Text>
                             <Controller
                                 control={control}
                                 name="name"
@@ -72,6 +97,13 @@ export function ModalLogon({ visible = false, onChangeVisibility }: ModalLogonPr
                             marginTop={4}
                             width={'100%'}
                         >
+                            <Text
+                                fontSize={'sm'}
+                                color={'red.400'}
+                                marginBottom={2}
+                            >
+                                {errors.name?.message}
+                            </Text>
                             <Controller
                                 control={control}
                                 name="username"
@@ -92,6 +124,13 @@ export function ModalLogon({ visible = false, onChangeVisibility }: ModalLogonPr
                             marginTop={4}
                             width={'100%'}
                         >
+                            <Text
+                                fontSize={'sm'}
+                                color={'red.400'}
+                                marginBottom={2}
+                            >
+                                {errors.name?.message}
+                            </Text>
                             <Controller
                                 control={control}
                                 name="password"
@@ -118,8 +157,9 @@ export function ModalLogon({ visible = false, onChangeVisibility }: ModalLogonPr
                                 width={'100%'}
                                 colorScheme={'pink'}
                                 size={'lg'}
+                                isLoading={isLoading}
                             >
-                                Entrar
+                                Salvar
                             </Button>
                         </Box>
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './global/theme';
 import { Routes } from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +13,20 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Routes />
+      <ToastContainer />
     </ChakraProvider>
   </React.StrictMode>
 );
